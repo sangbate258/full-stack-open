af8ec3f9e3cad34bf45b6c3254d089e8a766ed4e
Duong Ba Sang <sangbate258@gmail.com>
2026-09-19 15:50:49 +0700

    1.12-1.14: anecdotes

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
+
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
+  if (all === 0)
+    return (
+      <>
+        <h1>statistics</h1>
+        <h2>No feedback given</h2>
+      </>
+    );
   const average = (good - bad) / all;
   const positive = (good / all) * 100;
 
-  if (all === 0) return <h2>No feedback given</h2>;
   return (
     <div>
       <h1>statistics</h1>
@@ -37,6 +44,7 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  console.log("render app:", good, neutral, bad);
   const handleGood = () => {
     setGood(good + 1);
   };
